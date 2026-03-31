14286bc51de80a7765d7100a106b99629ebf9231
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-03-31 23:59:20 +0500

    add app to settings, fix AppConfig name and define initial models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, unique=True, verbose_name='Name of Category')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+class Post(models.Model):
+    title = models.CharField(max_length=500,unique=True, verbose_name='Name of Post')
+    content = models.TextField(verbose_name='Content')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created Date')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated Date')
+    is_publish = models.BooleanField(default=True, verbose_name='Publish')
+    views = models.IntegerField(default=0, verbose_name='Views')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Category')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
