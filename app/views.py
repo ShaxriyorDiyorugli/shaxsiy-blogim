f6dba2598ddda9a01cd887df0ea90639001474e4
ShaxriyorDiyorugli <shaxriyordiyorugli@gmail.com>
2026-04-01 01:04:56 +0500

    feat: add initial views and urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(is_publish=True).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'posts/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.views += 1
+    post.save()
+
+    context = {
+        'post': post
+    }
+    return render(request, 'posts/post_detail.html', context)
